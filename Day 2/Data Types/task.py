fbdb161336bca1ad907caf1c9c4a475f1f57552b
aisha <aishaprof34@gmail.com>
2024-09-06 17:05:55 +0530

    hi

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,18 @@
+#Subscripting
+print("Hello"[0])
+
+#String
+print("123" + "456")
+
+#Integer = Whole number
+print(123+345)
+
+#Large Integers
+print(123_456_789)
+
+#Float = Floating point number
+print(3.345676)
+
+#Boolean
+print(True)
+print(False)
